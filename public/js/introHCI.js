bda0f0b2b3951aab64c4fee397db861dfe458c44
Timothy Santos <tsantos@stanford.edu>
2014-02-26 23:21:59 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,11 +3,15 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function() {
+		ga("send", "event", "like", "click");
+	});
 }
+
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
+	projects.grid = false;
+  	res.render('index', {'allprojects': projects, 'grid': false});
+};
+
+exports.viewGrid = function(req, res){
+  	res.render('index', {'allprojects': projects, 'grid': true});
 };
